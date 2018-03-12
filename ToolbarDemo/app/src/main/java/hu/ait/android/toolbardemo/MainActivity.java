3dc4127b5c3eaf4f6938d8133336dac5ebddd2ff
Péter Ekler <peter.ekler@gmail.com>
2018-03-12 13:48:02 +0100

    ToolbarDemo: menu events, Toolbar style LayoutDemo: LinearLayout, RelativeLayout

--- ToolbarDemo/app/src/main/java/hu/ait/android/toolbardemo/MainActivity.java
+++ ToolbarDemo/app/src/main/java/hu/ait/android/toolbardemo/MainActivity.java
@@ -3,6 +3,10 @@ package hu.ait.android.toolbardemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_start:
+                Toast.makeText(this,
+                        "Game has started",
+                        Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.action_help:
+                break;
+        }
+
+        return true;
     }
 }
